8ea8d1142ccfd4d5debd23ab6b64b8d04281c4f5
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-10-06 19:37:25 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -41,7 +41,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number);
+void (*op_func(char *token1))(stack_t **stack, unsigned int line_number);
 void push(stack_t **h, unsigned int line_number, const char *n);
 void pop(stack_t **h, unsigned int line_number);
 void pint(stack_t **h, unsigned int line_number);
